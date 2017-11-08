23a46b588a69d9f31d7fd5918484154ff1959f2c
Martin Mekota <martin.mekota@gmail.com>
2017-11-08 18:15:05 +0100

    Correct x and ys

--- test1.py
+++ test1.py
@@ -4,10 +4,10 @@ import numpy as np
 import pickle
 
 data = np.genfromtxt('/home/student/Downloads/train_data/alpine-1.csv', skip_header=1, dtype=float, delimiter=',', skip_footer=1)
-x_train = data[0:13600,0:3]
-y_train = data[0:13600,3:25]
-x_test = data[13600:,0:3]
-y_test = data[13600:,3:25]
+x_train = data[0:13600,3:25]
+y_train = data[0:13600,0:3]
+x_test = data[13600:,3:25]
+y_test = data[13600:,0:3]
 
 nn = MLPRegressor(
     hidden_layer_sizes=(28,28,28))
