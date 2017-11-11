45b99bc230fe88471cd4cc00b8fed5d3932dd47d
Jeremy Barisch Rooney <barischrooneyj@gmail.com>
2017-11-11 14:39:28 +0100

    Save model to file

--- keras-0.py
+++ keras-0.py
@@ -3,20 +3,22 @@ from keras.layers import Dense, Activation
 
 from data import all_data, x_y, split_data
 
+input_dim = 22
+
 
 # A simple NN model.
-def simple_NN(input_dim):
+def simple_NN():
     model = Sequential()
-    model.add(Dense(units=22, input_shape=(input_dim,)))
+    model.add(Dense(units=50, input_shape=(input_dim,)))
     model.add(Activation("relu"))
-    model.add(Dense(units=22))
+    model.add(Dense(units=25))
     model.add(Activation("relu"))
     model.add(Dense(units=3))
     model.add(Activation("softmax"))
     return model
 
 
-# A high level function that takes a model and evaluates it.
+# A high level function that takes a model, trains & saves it.
 def run_model(model):
     tr, va, te = split_data(all_data(), 5, 1, 1)
     x_train, y_train = x_y(tr)
@@ -28,8 +30,10 @@ def run_model(model):
     model.compile(optimizer="rmsprop", loss="mse", metrics=["accuracy"])
     model.fit(x_train, y_train, epochs=10, batch_size=32)
     loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
+    print(loss_and_metrics)
+    model.save("./models/keras.pickle")
 
 
 if __name__ == "__main__":
-    model = simple_NN(22)
+    model = simple_NN()
     run_model(model)
